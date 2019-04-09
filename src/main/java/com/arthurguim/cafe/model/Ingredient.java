76c357ff63c4e030f4fa9c9c2144484559023846
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-08 22:03:48 -0300

    Remake ingredient class and remove IngredientType

---
+++ src/main/java/com/arthurguim/cafe/model/Ingredient.java
@@ -0,0 +1,11 @@
+package com.arthurguim.cafe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Ingredient {
+	private String name;
+    private Double price;
+}
--- src/main/java/com/arthurguim/cafe/model/IngredientType.java
+++
@@ -1,5 +0,0 @@
-package com.arthurguim.cafe.model;
-
-public enum IngredientType {
-    LETTUCE, BACON, HAMBURGUER, EGG, CHEESE;
-}
