48e5d008116125fee1e05c323a452e2856a5b076
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-09 21:53:25 -0300

    Include ingredient validation

--- src/main/java/com/arthurguim/cafe/model/Ingredient.java
+++ src/main/java/com/arthurguim/cafe/model/Ingredient.java
@@ -1,11 +1,30 @@
 package com.arthurguim.cafe.model;
 
-import lombok.AllArgsConstructor;
+import java.util.Arrays;
+import java.util.List;
+
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Ingredient {
-	private String name;
+
+    // Ingredient name
+    private String name;
+
+    // Ingredient price
     private Double price;
+
+    // TODO: remove this list
+    private final List<String> VALID_INGREDIENTS = Arrays.asList("hamburguer", "cheese", "egg", "lettuce", "bacon");
+
+    public Ingredient(String name, Double price) throws Exception {
+        // Check if the name of the ingredient is registered
+        if (!VALID_INGREDIENTS.contains(name)) {
+            // TODO: throw right exception if ingredient is not valid
+            throw new Exception("Not valid");
+        }
+
+        this.name = name;
+        this.price = price;
+    }
 }
