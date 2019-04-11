df889c05c46b9c95e264b0b42496d9440bf2dcb2
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-10 22:34:12 -0300

    Include hamburguer model

---
+++ src/main/java/com/arthurguim/cafe/model/Hamburguer.java
@@ -0,0 +1,12 @@
+package com.arthurguim.cafe.model;
+
+import java.util.List;
+
+public class Hamburguer {
+
+    // Hamburguer name
+    private String name;
+
+    // Hamburguer ingredients
+    private List<Ingredient> ingredients;
+}
--- src/main/java/com/arthurguim/cafe/model/Ingredient.java
+++ src/main/java/com/arthurguim/cafe/model/Ingredient.java
@@ -1,8 +1,5 @@
 package com.arthurguim.cafe.model;
 
-import java.util.Arrays;
-import java.util.List;
-
 import lombok.Getter;
 
 @Getter
@@ -14,16 +11,7 @@ public class Ingredient {
     // Ingredient price
     private Double price;
 
-    // TODO: remove this list
-    private final List<String> VALID_INGREDIENTS = Arrays.asList("hamburguer", "cheese", "egg", "lettuce", "bacon");
-
-    public Ingredient(String name, Double price) throws Exception {
-        // Check if the name of the ingredient is registered
-        if (!VALID_INGREDIENTS.contains(name)) {
-            // TODO: throw right exception if ingredient is not valid
-            throw new Exception("Not valid");
-        }
-
+    public Ingredient(String name, Double price) {
         this.name = name;
         this.price = price;
     }
