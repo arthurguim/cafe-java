6a9f93bd73b0df6007d299620b84e3595bfd29e5
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-12 18:38:27 -0300

    Fix crash for database

--- src/main/java/com/arthurguim/cafe/cafe/CafeApplication.java
+++ src/main/java/com/arthurguim/cafe/cafe/CafeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.arthurguim.cafe.model"})
+@EntityScan(basePackages = {"com.arthurguim.cafe.cafe.model"})
 public class CafeApplication {
 
 	public static void main(String[] args) {
