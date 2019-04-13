525164d62f82ee90eb0dd1aba1eb7b47c48751a1
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-13 17:43:11 -0300

    Create quantitative test

--- src/test/java/com/arthurguim/cafe/cafe/CustomOrderTests.java
+++ src/test/java/com/arthurguim/cafe/cafe/CustomOrderTests.java
@@ -1,14 +1,23 @@
 package com.arthurguim.cafe.cafe;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import com.arthurguim.cafe.cafe.controller.Validator;
+import javax.persistence.EntityNotFoundException;
+
+import com.arthurguim.cafe.cafe.controller.IngredientValidator;
+import com.arthurguim.cafe.cafe.controller.SalesController;
 import com.arthurguim.cafe.cafe.model.Ingredient;
 import com.arthurguim.cafe.cafe.model.Order;
+import com.arthurguim.cafe.cafe.model.sales.QuantitativeSale;
+import com.arthurguim.cafe.cafe.repository.IngredientRepository;
+import com.arthurguim.cafe.cafe.repository.QuantitativeSaleRepository;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,20 +32,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CustomOrderTests {
 
 	@Autowired
-	private Validator validator;
+	private IngredientValidator ingredientValidator;
+
+	@Autowired
+	private QuantitativeSaleRepository quantitativeSaleRepository;
+
+	@Autowired
+	private IngredientRepository ingredientRepository;
+
+	@Autowired
+	private SalesController salesController;
 
 	private final List<String> VALID_INGREDIENTS = Arrays.asList("queijo", "hamburguer de carne", "bacon");
 	private final List<String> INVALID_INGREDIENTS = Arrays.asList("picles", "hamburguer", "bacon");
 
+	// Custom order with valid ingredients
 	@Test
 	public void creatingCustomOrderValidIngredients() throws Exception {
 		List<Ingredient> ingredients = new ArrayList<>();
 		for (String ingredientName : VALID_INGREDIENTS) {
-			Ingredient ingredient = validator.validateIngredient(ingredientName);
+			Ingredient ingredient = ingredientValidator.validateIngredient(ingredientName);
 			ingredient.setQuantity(1);
 			ingredients.add(ingredient);
 		}
-		
+
 		Order order = new Order(ingredients);
 
 		Double totalExpected = .0;
@@ -47,15 +66,58 @@ public class CustomOrderTests {
 		assertEquals(totalExpected, order.getTotalPrice());
 	}
 
+	// Custom order with invalid ingredients
 	@Test(expected = Exception.class)
 	public void creatingCustomOrderInvalidIngredients() throws Exception {
 		List<Ingredient> ingredients = new ArrayList<>();
 		for (String ingredientName : INVALID_INGREDIENTS) {
-			Ingredient ingredient = validator.validateIngredient(ingredientName);
+			Ingredient ingredient = ingredientValidator.validateIngredient(ingredientName);
 			ingredient.setQuantity(1);
 			ingredients.add(ingredient);
 		}
-		
+
 		new Order(ingredients);
+	}
+
+	// Custom with one quantitativeDiscount
+	@Test
+	public void creatingCustomOrderWithOneQuantitativeDiscount() throws Exception {
+		List<Ingredient> ingredients = new ArrayList<>();
+
+		// Get one quantitative sale
+		Optional<QuantitativeSale> quantitativeSalesOpt = quantitativeSaleRepository.findById(1L);
+
+		if(!quantitativeSalesOpt.isPresent()) {
+			fail("There was no quantitative sales on test database");
+		}
+
+		QuantitativeSale quantitativeSale = quantitativeSalesOpt.get();
+
+		// Get includant ingredient
+		Optional<Ingredient> ingredientOpt = ingredientRepository.findById(quantitativeSale.getIncludantIngredientId());
+
+		if(!ingredientOpt.isPresent()) {
+			fail("There was no ingredients on test database");
+		}
+
+		Ingredient ingredient = ingredientOpt.get();
+		ingredient.setQuantity(2);
+
+		ingredients.add(ingredient);
+
+		Order order = new Order(ingredients);
+		order = salesController.applySaleIfValid(order);
+
+		// Order original price
+		Double originalPrice = ingredient.getPrice() * ingredient.getQuantity();
+
+		// Order expectedPrice
+		Double expectedPrice = originalPrice * (1 - quantitativeSale.getSalePercentage() / 100);
+
+		// Order discount
+		Double discount = originalPrice - expectedPrice;
+
+		assertEquals(expectedPrice, order.getTotalPrice());
+		assertEquals(discount, order.getDiscount());
 	}
 }
