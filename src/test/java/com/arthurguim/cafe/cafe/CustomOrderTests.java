b163544208619f06c13e4ed08b6e1287d70775b4
Arthur Guimarães <arthurguim@hotmail.com>
2019-04-14 16:30:36 -0300

    Include multiple sales tests

--- src/test/java/com/arthurguim/cafe/cafe/CustomOrderTests.java
+++ src/test/java/com/arthurguim/cafe/cafe/CustomOrderTests.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.arthurguim.cafe.cafe.exception.IngredientNotFoundException;
+import com.arthurguim.cafe.cafe.controller.H2TestProfileConfig;
 import com.arthurguim.cafe.cafe.controller.SalesController;
 import com.arthurguim.cafe.cafe.model.Ingredient;
 import com.arthurguim.cafe.cafe.model.Order;
@@ -25,10 +26,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = CafeApplication.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = {CafeApplication.class, H2TestProfileConfig.class})
+@ActiveProfiles("test")
 public class CustomOrderTests {
 
 	@Autowired
@@ -167,4 +170,107 @@ public class CustomOrderTests {
 		assertEquals(StringUtils.EMPTY, order.getHamburguerName());
 		assertEquals(true, order.isCustomHamburguer());
 	}
+
+		// Custom with multiple quantitative discount
+		@Test
+		public void creatingCustomOrderWithMultipleQuantitativeDiscount() {
+			// List of ingredients to create custom order
+			List<Ingredient> ingredients = new ArrayList<>();
+			List<String> saleNames = new ArrayList<>();
+			Double originalPrice = .0;
+			int salePercentage = 0;
+
+			// Set includant and excludant ingredients for check
+			List<Long> includantIngredientIds = new ArrayList<>();
+			List<Long> excludantIngredientIds = new ArrayList<>();
+
+			// Get all quantitative sale
+			List<QuantitativeSale> quantitativeSales = quantitativeSaleRepository.findAll();
+
+			// Check if there's one or more quantitative sale on test database
+			if(quantitativeSales.size() <= 1) {
+				fail("There was no enough quantitative sales on test database");
+			}
+
+			// Get all ingredients from proportional sales
+			for (QuantitativeSale quantitativeSale : quantitativeSales) {
+				includantIngredientIds.add(quantitativeSale.getIncludantIngredientId());
+				excludantIngredientIds.add(quantitativeSale.getExcludantIngredientId());
+
+				Ingredient ingredient = ingredientRepository.findById(quantitativeSale.getIncludantIngredientId()).get();
+				ingredient.setQuantity(1);
+
+				originalPrice += ingredient.getPrice() * ingredient.getQuantity();
+				salePercentage += quantitativeSale.getSalePercentage();
+				saleNames.add(quantitativeSale.getName());
+				ingredients.add(ingredient);
+			}
+
+			// Check if there's any conflicts between the quantitative sales
+			if(!excludantIngredientIds.retainAll(includantIngredientIds)) {
+				fail("Quantitative test data is not valid: There's conflict between includant and excludant ingredients");
+			}
+
+			Order order = new Order(ingredients);
+			order = salesController.applySaleIfValid(order);
+
+			Double expectedPrice = originalPrice * (1 - salePercentage / 100);
+			expectedPrice = Precision.round(expectedPrice, 2);
+			Double discount = originalPrice - expectedPrice;
+			discount = Precision.round(discount, 2);
+
+			assertEquals(expectedPrice, order.getTotalPrice());
+			assertEquals(discount, order.getDiscount());
+			assertEquals(order.getSaleName(), saleNames);
+			assertEquals(StringUtils.EMPTY, order.getHamburguerName());
+			assertEquals(true, order.isCustomHamburguer());
+		}
+
+		// Custom with multiple proportional discount
+		@Test
+		public void creatingCustomOrderWithMultipleProportionalDiscount() {
+			// List of ingredients to create custom order, sales expected names, original and expected price
+			List<Ingredient> ingredients = new ArrayList<>();
+			List<String> saleNames = new ArrayList<>();
+			Double orderOriginalPrice = .0;
+			Double orderExpectedPrice = .0;
+
+			// Get all proportional sales
+			List<ProportionalSale> proportionalSales = proportionalSaleRepository.findAll();
+
+			// Check if there's one or more proportional sale on test database
+			if(proportionalSales.size() <= 1) {
+				fail("There was no enough proportional sales on test database");
+			}
+
+			// Get all ingredients from proportional sales
+			for (ProportionalSale proportionalSale : proportionalSales) {
+				Ingredient ingredient = ingredientRepository.findById(proportionalSale.getIngredientId()).get();
+
+				// Set proportional quantity
+				ingredient.setQuantity(proportionalSale.getProportion());
+
+				// Calculate original and expected price
+				orderOriginalPrice += ingredient.getPrice() * ingredient.getQuantity();
+				orderExpectedPrice += ingredient.getPrice() * (ingredient.getQuantity() - 1);
+
+				// Include sale name
+				saleNames.add(proportionalSale.getName());
+
+				// Include ingredient
+				ingredients.add(ingredient);
+			}
+
+			Order order = new Order(ingredients);
+			order = salesController.applySaleIfValid(order);
+
+			// Ingredient discount
+			Double discount = orderOriginalPrice - orderExpectedPrice;
+
+			assertEquals(saleNames, order.getSaleName());
+			assertEquals(orderExpectedPrice, order.getTotalPrice());
+			assertEquals(discount, order.getDiscount());
+			assertEquals(StringUtils.EMPTY, order.getHamburguerName());
+			assertEquals(true, order.isCustomHamburguer());
+		}
 }
